0a88ee73e5dbcc3b20e016bbf18b6772f35a5c51
Vitalii Orazov <viordash@yandex.ru>
2025-08-17 15:18:04 +0000

    metrics: fix CountParametersMetric to

--- src/metric_impl/parameters_count.cpp
+++ src/metric_impl/parameters_count.cpp
@@ -1,4 +1,5 @@
 #include "metric_impl/parameters_count.hpp"
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -6,6 +7,24 @@
 #include <string_view>
 
 namespace analyser::metric::metric_impl {
+
+namespace {
+size_t GetParamsBlockIndent(std::string_view params) {
+    auto end_line = params.find('\n');
+    if (end_line == std::string_view::npos) {
+        throw std::invalid_argument{"Invalid AST, params w/o end of line"};
+    }
+    end_line += sizeof('\n');
+
+    size_t indent = 0;
+    auto it = params.begin() + end_line;
+    while (*it == ' ' && it != params.end()) {
+        it++;
+        indent++;
+    }
+    return indent;
+}
+}  // namespace
 
 std::string CountParametersMetric::Name() const { return "ParametersCount"; }
 
@@ -22,11 +41,17 @@ MetricResult::ValueType CountParametersMetric::CalculateImpl(const function::Fun
 
     std::string_view params(f.ast.data() + params_start + 1, params_end - params_start - 1);
 
-    const std::string_view identifier = "(identifier";
+    auto params_end_line = params.find('\n');
+    if (params_end_line == std::string::npos) {
+        throw std::invalid_argument{"Invalid AST, no end of line"};
+    }
+
+    auto level_indent = GetParamsBlockIndent(params);
+    const std::string param_id = std::format("\n{:{}}(", "", level_indent);
     size_t params_count = 0;
     size_t pos = 0;
-    while ((pos = params.find(identifier, pos)) != std::string_view::npos) {
-        pos += identifier.length();
+    while ((pos = params.find(param_id, pos)) != std::string_view::npos) {
+        pos += param_id.length();
         params_count++;
     }
     return params_count;
--- src/metric_impl/tests/parameters_count.cpp
+++ src/metric_impl/tests/parameters_count.cpp
@@ -29,7 +29,8 @@ TEST(CountParametersMetricTests, no_parameters) {
 TEST(CountParametersMetricTests, one_simple_parameter) {
     CountParametersMetric metric;
     auto func = function::Function{.ast = R"((function_definition
-        parameters: (parameters (identifier))
+        parameters: (parameters 
+          (identifier))
         body: (block)))"};
 
     auto result = metric.Calculate(func);
@@ -38,12 +39,21 @@ TEST(CountParametersMetricTests, one_simple_parameter) {
 
 TEST(CountParametersMetricTests, multiple_mixed_parameters) {
     CountParametersMetric metric;
-    auto func = function::Function{.ast = R"((function_definition
-        parameters: (parameters
-            (identifier)
-            (typed_parameter (identifier) type: (type))
-            (typed_default_parameter (identifier) type: (type) value: (float)))
-        body: (block)))"};
+    auto func = function::Function{.ast = R"((function_definition [3, 4] - [5, 28]
+        name: (identifier [3, 8] - [3, 16])
+        parameters: (parameters [3, 16] - [3, 68]
+          (identifier [3, 17] - [3, 21])
+          (typed_default_parameter [3, 23] - [3, 46]
+            name: (identifier [3, 23] - [3, 33])
+            type: (type [3, 35] - [3, 40]
+              (identifier [3, 35] - [3, 40]))
+            value: (float [3, 43] - [3, 46]))
+          (typed_default_parameter [3, 48] - [3, 67]
+            name: (identifier [3, 48] - [3, 54])
+            type: (type [3, 56] - [3, 61]
+              (identifier [3, 56] - [3, 61]))
+            value: (float [3, 64] - [3, 67])))
+        body: (block [4, 8] - [5, 28])"};
 
     auto result = metric.Calculate(func);
     ASSERT_EQ(result.value, 3);
