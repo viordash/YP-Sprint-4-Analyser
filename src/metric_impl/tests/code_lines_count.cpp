b1b74119cc9e86ab77dd4a1696d9d5ffda4e5a29
Vitalii Orazov <viordash@yandex.ru>
2025-08-17 21:11:58 +0000

    tests: use tests files for CodeLinesCountMetric

--- src/metric_impl/tests/code_lines_count.cpp
+++ src/metric_impl/tests/code_lines_count.cpp
@@ -1,6 +1,7 @@
 #include "metric_impl/code_lines_count.hpp"
 
 #include <gtest/gtest.h>
+#include <string>
 
 namespace analyser::metric::metric_impl {
 
@@ -12,14 +13,6 @@ TEST(CodeLinesCountMetricTests, get_metric_name) {
     ASSERT_EQ(result.metric_name, "CodeLinesCount");
 }
 
-TEST(CodeLinesCountMetricTests, multiline_function) {
-    CodeLinesCountMetric metric;
-    auto func = function::Function{.ast = "(function_definition [10, 0] - [14, 15] body: (block ...))"};
-
-    auto result = metric.Calculate(func);
-    ASSERT_EQ(result.value, 5);
-}
-
 TEST(CodeLinesCountMetricTests, singleline_function) {
     CodeLinesCountMetric metric;
     auto func = function::Function{
@@ -29,39 +22,42 @@ TEST(CodeLinesCountMetricTests, singleline_function) {
     ASSERT_EQ(result.value, 1);
 }
 
-TEST(CodeLinesCountMetricTests, multiline_docstring) {
-    CodeLinesCountMetric metric;
-    auto func = function::Function{.ast = R"((function_definition [20, 0] - [25, 21]
-        name: (identifier)
-        parameters: (parameters)
-        body: (block
-            (expression_statement (string))
-            (return_statement (true)))))"};
-
-    auto result = metric.Calculate(func);
-
-    ASSERT_EQ(result.value, 6);
-}
-
-TEST(CodeLinesCountMetricTests, comments_are_ignored) {
-    CodeLinesCountMetric metric;
-    auto func = function::Function{.ast = R"((function_definition [20, 0] - [30, 21]
-        name: (identifier)
-        parameters: (parameters)
-        body: (block
-            (comment [22, 4] - [22, 46])
-            (expression_statement (string))
-            (comment [25, 4] - [25, 54])
-            (return_statement (true)))))"};
-
-    auto result = metric.Calculate(func);
-    ASSERT_EQ(result.value, 9);
-}
-
 TEST(CodeLinesCountMetricTests, malformed_ast_throws_invalid_argument) {
     CodeLinesCountMetric metric;
     auto func = function::Function{.ast = "(function_definition body: (block ...))"};
 
     EXPECT_THROW(metric.Calculate(func), std::invalid_argument);
 }
+
+class CodeLinesCountFromTestFilesTest : public ::testing::TestWithParam<std::tuple<std::string, int>> {};
+
+TEST_P(CodeLinesCountFromTestFilesTest, from_test_file) {
+    auto filename = std::get<0>(GetParam());
+    auto count = std::get<1>(GetParam());
+
+    CodeLinesCountMetric metric;
+    function::FunctionExtractor func_extractor;
+    auto file = file::File(std::get<0>(GetParam()));
+
+    auto func = func_extractor.Get(file);
+    ASSERT_EQ(func.size(), 1);
+    auto result = metric.Calculate(func[0]);
+    ASSERT_EQ(result.value, std::get<1>(GetParam()));
+}
+
+const static std::string filepath = "../src/metric_impl/tests/files/";
+INSTANTIATE_TEST_SUITE_P(_, CodeLinesCountFromTestFilesTest,
+                         ::testing::Values(                                        //
+                             std::make_tuple(filepath + "comments.py", 4),         //
+                             std::make_tuple(filepath + "exceptions.py", 8),       //
+                             std::make_tuple(filepath + "if.py", 4),               //
+                             std::make_tuple(filepath + "loops.py", 7),            //
+                             std::make_tuple(filepath + "many_lines.py", 11),      //
+                             std::make_tuple(filepath + "many_parameters.py", 2),  //
+                             std::make_tuple(filepath + "match_case.py", 8),       //
+                             std::make_tuple(filepath + "nested_if.py", 9),        //
+                             std::make_tuple(filepath + "simple.py", 6),           //
+                             std::make_tuple(filepath + "ternary.py", 2)           //
+                             ));
+
 }  // namespace analyser::metric::metric_impl
